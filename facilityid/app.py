68ded086380695983617b8b4460c5f2f23041263
= <nestlerj@bouldercolorado.gov>
2019-09-15 14:13:32 -0600

    Adds TODO items to app.py based on v1.0 of FacilityID

---
+++ facilityid/app.py
@@ -0,0 +1,4 @@
+# TODO: place globals into yml config file
+# TODO: transfer items from personal modules into a project utilities module
+# TODO: figure out how to download the boulder_gis package into conda env
+# TODO: move database connections into config file
