7f254086163b45cf60c0551f1f5ad0abeb0f05f8
= <nestlerj@bouldercolorado.gov>
2019-09-15 18:12:25 -0600

    Adds basic configuration to logging

--- facilityid/__main__.py
+++ facilityid/__main__.py
@@ -1,4 +1,11 @@
 from facilityid import app
+import logging
+
+logging.basicConfig(filename='app.log',
+                    level=logging.INFO,
+                    format='%(asctime)s : %(levelname)s : %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+
 
 if __name__ == "__main__":
     app.main()
