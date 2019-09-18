fb1010d55eadaf81394e1a876ddad1c44921b405
Jesse Nestler <jesse.nestler@colorado.edu>
2019-09-17 21:30:32 -0600

    Simplifies the delete_facilityid_versions function

--- facilityid/utils/management.py
+++ facilityid/utils/management.py
@@ -2,15 +2,12 @@ from arcpy import da
 from arcpy import DeleteVersion_management
 
 
-def delete_facilityid_versions(connection: str = None, users: list = None) -> None:
+def delete_facilityid_versions(connection: str = None) -> None:
     """Deletes versions created for editing Facility IDs
 
     :param connection: location of the sde connection file
-    :param users: list of users with Facility ID versions
     :return: None
     """
-    sde_versions = [v.name for v in da.ListVersions(connection)]
-    for u in users:
-        delete_version = "%s%s" % (u, "_FacilityID")
-        if delete_version in sde_versions:
-            DeleteVersion_management(connection, delete_version)
+    del_versions = [v.name for v in da.ListVersions(connection) if "FacilityID" in v.name]
+    for d in del_versions:
+        DeleteVersion_management(connection, d)
