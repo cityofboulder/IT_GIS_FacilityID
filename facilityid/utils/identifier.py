84f07f9706479af3c94f4e8a3d5f68959f78d413
Jesse Nestler <jesse.nestler@colorado.edu>
2019-09-25 18:01:45 -0600

    Modifies docstrings; Adds has_table() and get_rows() to Identifier

--- facilityid/utils/identifier.py
+++ facilityid/utils/identifier.py
@@ -1,4 +1,5 @@
 from arcpy import ArcSDESQLExecute, Describe, ExecuteError, GetCount_management, ListFields
+from arcpy.da import SearchCursor
 from os import path
 
 
@@ -26,16 +27,41 @@ class Identifier(ArcSDESQLExecute):
             return None
 
     def has_facilityid(self):
-        """Determines if the feature class has the FACILITYID field."""
+        """Determines if the feature class has the FACILITYID field.
+
+        :return: Boolean, True if FACILITYID is a field in the SDE item
+        """
         return True if "FACILITYID" in self.fields else False
 
     def has_globalid(self):
-        """Determines if the feature class has the GLOBALID field."""
+        """Determines if the feature class has the GLOBALID field.
+
+        :return: Boolean, True if the SDE item has a GLOBALID field
+        """
         return True if "GLOBALID" in self.fields else False
+
+    def has_table(self):
+        """Identifies whether an item in the SDE is a feature class or table
+
+        :return: Boolean, true if the item is a feature class or table
+        """
+        return True if self._desc.dataType in ['FeatureClass', 'Table'] else False
 
     def find_prefix(self):
         """Determines the prefix of the feature class based on the most prevalent occurrence."""
         # TODO: fill in code
         return True
 
-    # TODO: Add a function that converts each row of the fc to a dict {"GLOBALID": , "FACILITYID": , "CREATED": , "EDITED": }
+    def get_rows(self):
+        """Opens an arcpy SearchCursor and records fields relevant to the QC of FACILITYID
+
+        :return: List, rows represented as dictionaries within a list
+        """
+        fields = ['GLOBALID', 'FACILITYID', 'CREATED_USER', 'CREATED_DATE', 'LAST_EDITED_USER', 'LAST_EDITED_DATE']
+        if self._desc.dataType == 'FeatureClass':
+            fields.append('SHAPE@')
+        row_list = []
+        with SearchCursor(self.full_path, fields) as search:
+            for row in search:
+                row_list.append({fields[i]: row[i] for i in range(len(row))})
+        return row_list
