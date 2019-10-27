a537f4cc7238b8e9d7a34d9972de68ca19236056
Jesse Nestler <jesse.nestler@colorado.edu>
2019-10-27 14:56:20 -0600

    Adds code to find_prefix method using SQL

--- facilityid/utils/identifier.py
+++ facilityid/utils/identifier.py
@@ -1,23 +1,29 @@
-from arcpy import ArcSDESQLExecute, Describe, ExecuteError, GetCount_management, ListFields
-from arcpy.da import SearchCursor
-from os import path
+from arcpy import ArcSDESQLExecute, ExecuteError, GetCount_management, ListFields
+from arcpy.da import SearchCursor, Describe
+import os
 
 
 class Identifier:
     """A class intended to deal with the specifics of controlling for the quality of Facility IDs. This builds upon the
     Describe object in arcpy."""
-    def __init__(self, iterator_path):
-        self._desc = Describe(self.full_path)
-        self.fields = [f.name for f in ListFields(self.full_path) if not f.required]
-        self.full_path = path.join(*iterator_path)
-        self.owner, self.name = iterator_path[-1].split(".")
+    def __init__(self, tuple_path):
+        self.tuple_path = tuple_path
+        self.full_path = os.path.join(*self.tuple_path)
+        self.fields = [f.name for f in ListFields(
+            self.full_path) if not f.required]
+        self.connection = self.tuple_path[0]
+        self.dataset = self.tuple_path[1] if len(
+            self.tuple_path) == 3 else None
+        self.owner, self.name = tuple_path[-1].split(".")
         self.prefix = self.find_prefix()
+
+        self._desc = Describe(self.full_path)
 
     def __getattr__(self, item):
         """Pass any other attribute or method calls through to the underlying Describe object"""
         return getattr(self._desc, item)
 
-    def has_records(self):
+    def has_records(self) -> bool:
         """Determines if there are any records in the feature class to analyze."""
         try:
             count = int(GetCount_management(self.full_path).getOutput(0))
@@ -26,32 +32,48 @@ class Identifier:
             # TODO: Add info logging describing an error in retrieving a feature count
             return None
 
-    def has_facilityid(self):
+    def has_facilityid(self) -> bool:
         """Determines if the feature class has the FACILITYID field.
 
-        :return: Boolean, True if FACILITYID is a field in the SDE item
+        :return: Boolean
         """
         return True if "FACILITYID" in self.fields else False
 
-    def has_globalid(self):
+    def has_globalid(self) -> bool:
         """Determines if the feature class has the GLOBALID field.
 
         :return: Boolean, True if the SDE item has a GLOBALID field
         """
         return True if "GLOBALID" in self.fields else False
 
-    def has_table(self):
+    def has_table(self) -> bool:
         """Identifies whether an item in the SDE is a feature class or table
 
         :return: Boolean, true if the item is a feature class or table
         """
-        return True if self._desc.dataType in ['FeatureClass', 'Table'] else False
+        return True if self._desc.datasetType in ['FeatureClass', 'Table'] else False
 
     def find_prefix(self):
         """Determines the prefix of the feature class based on the most prevalent occurrence."""
-        # TODO: find the prefix on the SQL level using regex or similar to split lines
+        if self.has_facilityid():
+            # Initialize an executor object for SDE
+            execute_object = ArcSDESQLExecute(self.connection)
+            query_name = ".".join([self.owner, self.name[:26]]) + "_EVW" if self.isVersioned else ".".join(
+                [self.owner, self.name[:30]])
+            query = f"""SELECT REGEXP_SUBSTR(FACILITYID, '^[a-zA-Z]+') as PREFIXES,
+                    COUNT(*) as PFIXCOUNT
+                    FROM {query_name}
+                    GROUP BY REGEXP_SUBSTR(FACILITYID, '^[a-zA-Z]+')
+                    ORDER BY PFIXCOUNT DESC
+                    FETCH FIRST ROW ONLY"""
+            try:
+                result = execute_object.execute(query)
+                return result[0]
+            except (ExecuteError, TypeError, AttributeError):
+                return None
+        else:
+            return None
         # TODO: pickle and shelve list of prefixes after every script run
-        return True
 
     def get_rows(self):
         """Opens an arcpy SearchCursor and records fields relevant to the QC of FACILITYID
@@ -67,7 +89,7 @@ class Identifier:
                 row_list.append({fields[i]: row[i] for i in range(len(row))})
         return row_list
 
-    def gisscr_can_edit(self, edit_connection):
+    def gisscr_can_edit(self, edit_connection) -> bool:
         """Reveals if the feature class is editable through the GISSCR connection.
 
         :param edit_connection: File path to a gisscr SDE connection
